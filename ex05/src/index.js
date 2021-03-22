43719fe3d515e6643f14e0bc4432e47832ac7ee4
EP-84 <edmir.prasovic@gmail.com>
2021-03-22 05:04:50 +0100

    v2

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,35 +1,29 @@
-// Only change code below this line
+function myPetsFunction(pets) {
+        
+    return pets;
+}
 
 var myPetsArray = [
-    { 
-         animalType: "Dog",
-         name: "Pujdo"  
-    },
-    { 
-        animalType: "Cat",
-        name: "Maca", 
-    },  
-    { 
-        animalType: "Bird",
-        name: "Tweety",  
-    }
-    ];
+{ 
+     animalType: "Dog",
+     name: "Pujdo", 
+},
+{ 
+    animalType: "Cat",
+    name: "Maca",  
+},
     
-    myPetsArray[1].name[0];
-    
-    yPetsArray = myPetsArray[1].name[0];
-    
-
-    function myPetsFunction(pets) {
-     
-     return myPetsArray;
-    
-    
-    };
+{ 
+    animalType: "Bird",
+    name: "Tweety",   
    
+}
+];
 
-// Only change code below this line
-console.log(myPetsFunction(myPetsArray)); // Change this line
+myPetsArray[1].name[0];
+
+
+   
+console.log(myPetsFunction(myPetsArray));
 module.exports = myPetsFunction;
-module.exports.myPets = myPetsArray;
-
+module.exports.myPets = myPetsArry;
