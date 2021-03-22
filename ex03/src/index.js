123517491d732259550b3e6ef0fa5c98e536a30f
EP-84 <edmir.prasovic@gmail.com>
2021-03-22 04:29:39 +0100

    v2

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,24 +1,25 @@
-function rangeOfNumber(startN, endN) {
+// Only change code below this line
+function rangeOfNumbers(startN, endN) {
     if (startN === endN) { return [startN]        
 
     }      
     
-    else if (startN > endN) {return ["The starting number will always be less than or equal to the ending number"]}
+    else if (startN > endN) {return "The starting number will always be less than or equal to the ending number"}
     
     
     
     else {
         
-        const countArry = rangeOfNumber(startN, endN - 1)  
+        const countArry = rangeOfNumbers(startN, endN - 1)  
         countArry.push(endN);
         return countArry;
         
       
     }
 }
-
-console.log(rangeOfNumber(1, 7));
-console.log(rangeOfNumber(3, 10));
-console.log(rangeOfNumber(5, 5));
-console.log(rangeOfNumber(7, 5));
-module.exports = rangeOfNumber;
+// Only change code above this line
+console.log(rangeOfNumbers(1, 7)); // Change this line
+console.log(rangeOfNumbers(3, 10)); // Change this line
+console.log(rangeOfNumbers(5, 5)); // Change this line
+console.log(rangeOfNumbers(7, 5)); // Change this line
+module.exports = rangeOfNumber; // Change this line
