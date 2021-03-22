daa2f8c7ed4c14c21fd02488604e266cea31b7ff
EP-84 <edmir.prasovic@gmail.com>
2021-03-22 04:22:48 +0100

    v2

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,9 +1,13 @@
 function myRecursion(arr, n) {
-    var sum = 0;
-    for (var i = 0; i < n; i++) {
-        sum += arr[i];
+    
+    if (n <= 0) {
+
+        return 0;
+
+    } else {
+    
+        return  myRecursion(arr, n - 1) + arr[n-1]
     }
-    return sum;
 }
 console.log(myRecursion([1], 0));
 console.log(myRecursion([1, 2, 3, 4], 2));
